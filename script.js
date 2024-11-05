28dca8244fbd04e4baf93e51b5e07c20a5557fca
Kyle <61264128+kylef7@users.noreply.github.com>
2024-11-05 13:19:38 -0500

    Bell notification UI, sticky sidebar

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const root = document.querySelector(':root');
-const btn = document.querySelector('.bell');
+const btn = document.querySelector('.slider');
+const bellBtn = document.querySelector('.bell');
 
 let amount = 0;
+let theRoot = document.documentElement;
 
 document.querySelector(".sliderContainer").addEventListener('click', () => {
     if (amount % 2 == 0) {
@@ -14,3 +16,33 @@ document.querySelector(".sliderContainer").addEventListener('click', () => {
 
     amount++
 })
+
+let bellAmount = 0;
+
+bellBtn.addEventListener('click', () => {
+    if (bellAmount % 2 == 0) {
+    theRoot.style.setProperty('--notification-pop', "visible");
+    } else {
+        theRoot.style.setProperty('--notification-pop', "hidden");  
+    }
+
+    let change = getPosition(bellBtn);
+    theRoot.style.setProperty('--popdown-position', change.x + "px");
+
+    bellAmount++;
+});
+
+function getPosition(element) {
+    var rect = element.getBoundingClientRect();
+    return {
+        x: rect.left,
+        y: rect.top
+    };
+}
+
+
+
+window.addEventListener('resize', () => {
+    let change = getPosition(bellBtn);
+    theRoot.style.setProperty('--popdown-position', change.x + "px");
+});
