5a274b52427600541eaed711178c28ff1567089b
Kyle <61264128+kylef7@users.noreply.github.com>
2024-11-05 11:05:10 -0500

    Light/Dark mode implementation

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ const btn = document.querySelector('.bell');
 
 let amount = 0;
 
-btn.addEventListener('click', () => {
+document.querySelector(".sliderContainer").addEventListener('click', () => {
     if (amount % 2 == 0) {
         root.classList.add('dark');
+        btn.classList.add('moveForward');
     } else {
         root.classList.remove('dark');
+        btn.classList.remove('moveForward');
     }
 
     amount++
